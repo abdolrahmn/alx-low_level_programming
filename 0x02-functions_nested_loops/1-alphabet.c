1c300262755c9b083ec4cf9dc06ae0e4bcfdd7ce
abdolrahmn <segiuron79@gmail.com>
2022-10-18 15:33:14 -0700

    0,1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print the alpha in lowercase
+ */
+void print_alphabet(void)
+{
+	char la;
+
+	for (ls = 'a'; la <= 'z'; la++)
+		_putchar(la);_putchar(la);
+
+	_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islwer(int c);
+int _isalpha(int c);
+int print_sign(int c);
+int _abs(int c);
+int print_last_digit(int c);
+void jack_bauer(void);
+void time_table(void);
+int add(int, int);
+void print_to98(void);
+
+
+#endif
