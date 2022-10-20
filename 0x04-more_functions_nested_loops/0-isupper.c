ff8f71006cd77b0968a5ffe7188a5a97fa75c2b2
abdolrahmn <113601844+abdolrahmn@users.noreply.github.com>
2022-10-20 22:58:46 +0200

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -17,5 +17,23 @@ int _isupper(int c)
 	return (0);
 }
 
+}#include "main.h"
+/**
+* _isupper - checks for uppercase character
+* @c: Variable text
+* Return: Always 0.
+*/
+int _isupper(int c)
+{
+
+
+	if (c >= 'A' && c <= 'Z')
+	{
+	return (1);
+	}
+	else
+{
+	return (0);
 }
-Footer
+
+}
