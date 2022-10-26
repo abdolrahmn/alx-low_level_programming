071a48556decb8ba666231a005aac6ff86d572e2
abdolrahmn <113601844+abdolrahmn@users.noreply.github.com>
2022-10-27 01:22:27 +0200

    Update 104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,3 +1,5 @@
+
+
 #include "main.h"
 #include <stdio.h>
 
@@ -47,11 +49,4 @@ void print_buffer(char *b, int size)
 		printf("\n");
 		o += 10;
 	}
-}
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-Security
 }
